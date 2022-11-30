5004feb0859218d4bd84f6c452dacea3b30024b6
muhammad-abdul-rehman <muhammad_abdul_rehman@nextpak.org>
2022-11-30 07:38:46 +0500

    Prettier and Husky Added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import { Provider } from 'react-redux';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import Auth from './layout/Auth';
-import Main from './layout/Main';
-import routes from './routes';
-import store from './store';
+import { Provider } from "react-redux";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Auth from "./layout/Auth";
+import Main from "./layout/Main";
+import routes from "./routes";
+import store from "./store";
 
 function App() {
-	return (
-		<>
-			<Provider store={store}>
-				<BrowserRouter>
-					<Switch>
-						{routes.map(route => {
-							switch (route.layout) {
-								case 'main':
-									return (
-										<Route exact path={route.path}>
-											<Main>
-												<route.component />
-											</Main>
-										</Route>
-									);
-								case 'auth':
-									return (
-										<Route exact path={route.path}>
-											<Auth>
-												<route.component />
-											</Auth>
-										</Route>
-									);
-							}
-						})}
-						<Redirect to="/"/>
-					</Switch>
-				</BrowserRouter>
-			</Provider>
-		</>
-	);
+    return (
+        <>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        {routes.map((route) => {
+                            switch (route.layout) {
+                                case "main":
+                                    return (
+                                        <Route exact path={route.path}>
+                                            <Main>
+                                                <route.component />
+                                            </Main>
+                                        </Route>
+                                    );
+                                case "auth":
+                                    return (
+                                        <Route exact path={route.path}>
+                                            <Auth>
+                                                <route.component />
+                                            </Auth>
+                                        </Route>
+                                    );
+                            }
+                        })}
+                        <Redirect to="/" />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        </>
+    );
 }
 
 export default App;
